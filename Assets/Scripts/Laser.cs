229c37fb2248fd328d2b0c604009392be1bb14d9
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2020-12-04 15:11:41 +0100

    Added Laser Fire handling with specific material

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 8;
+
+    [SerializeField]
+    float _maxHeight = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the laser up
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        // Auto-destruction
+        if(transform.position.y > _maxHeight)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Speed velocity unit/second.
-    [SerializeField]
-    float _speed = 5.0f;
-
-    [SerializeField]
-    float _yBound = 9.0f;
-
-    [SerializeField]
-    float _xBound = 11.0f;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +14,23 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        CalculateMovements();
+        HandleLaserShooting();
+    }
+
+    // Speed velocity unit/second.
+    [SerializeField]
+    float _speed = 10f;
+
+    [SerializeField]
+    float _yBound = 6.7f;
+
+    [SerializeField]
+    float _xBound = 11.3f;
+
+    // Calculate movements of the player
+    void CalculateMovements()
     {
         // Get input using unity input manager (See Windows menu --> InputManager --> Axis)
         float horizontalAxis = Input.GetAxis("Horizontal");
@@ -42,6 +50,34 @@ public class Player : MonoBehaviour
 
         // Apply the new position
         transform.position = new Vector3(xPos, yPos, 0);
-
     }
+
+    // Laser prefab reference
+    [SerializeField]
+    GameObject _laserPrefab;
+
+    [SerializeField]
+    Vector3 _laserSpawnOffset = Vector3.zero;
+
+    [SerializeField]
+    float _fireRate = 0.5f;
+
+    // The next time you can shot
+    float _nextFire = 0f;
+
+    // Handle laser shooting
+    void HandleLaserShooting()
+    {
+        // Hit the space key, we spown the laser
+        if(Input.GetKey(KeyCode.Space) && Time.time > _nextFire)
+        {
+            // Calculate the next available time to shot, relative to the fire rate
+            _nextFire = Time.time + _fireRate;
+
+            // Spawn the laser in the current positon of the player
+            GameObject laser = GameObject.Instantiate(_laserPrefab, transform.position + _laserSpawnOffset, Quaternion.identity);
+
+        }
+    }
+
 }
