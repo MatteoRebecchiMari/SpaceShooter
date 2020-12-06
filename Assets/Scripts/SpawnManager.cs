08a763a3acb752596c2aa701d22bb3cd7ad08d94
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2020-12-06 15:58:36 +0100

    Added the powerup into the spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,10 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     GameObject _enemyPrefab;
 
+    // Prefab for the enemy
+    [SerializeField]
+    GameObject _powerUpPrefab;
+
     [SerializeField]
     float _yBound = 6.7f;
 
@@ -16,11 +20,13 @@ public class SpawnManager : MonoBehaviour
     float _xBound = 11.3f;
 
     Coroutine _spawnEnemies;
+    Coroutine _spawnPowerup;
 
     // Start is called before the first frame update
     void Start()
     {
         _spawnEnemies = StartCoroutine(SpawnEnemiesRoutine());
+        _spawnPowerup = StartCoroutine(SpawnPowerUpRoutine());
     }
 
     // Update is called once per frame
@@ -29,15 +35,13 @@ public class SpawnManager : MonoBehaviour
         
     }
 
+
+    // When the object is destroyed we ensure to stop coroutines
     void OnDestroy()
     {
-        StopCoroutine(_spawnEnemies);
+        StopSpawning();
     }
-
-    public void StopSpawning()
-    {
-        StopCoroutine(_spawnEnemies);
-    }
+ 
 
     // Coroutine to spawn enemies
     IEnumerator SpawnEnemiesRoutine()
@@ -48,7 +52,7 @@ public class SpawnManager : MonoBehaviour
         while (true)
         {
             // suspend execution for X seconds
-            float waitTimeSec = Random.Range(1f, 3f);
+            float waitTimeSec = Random.Range(1, 5f);
             yield return new WaitForSeconds(waitTimeSec);
 
             // Spawn the enemy
@@ -61,5 +65,34 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+
+    // Coroutine to spawn powerups elements
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        // Skip 1 frame
+        yield return null;
+
+        while (true)
+        {
+            // suspend execution for X seconds
+            float waitTimeSec = Random.Range(7f, 10f);
+            yield return new WaitForSeconds(waitTimeSec);
+
+            // Spawn the enemy
+            // Random x Position
+            float xPos = Random.Range(-_xBound, +_xBound);
+
+            // Generate the enemy in the random position
+            Instantiate(_powerUpPrefab, new Vector3(xPos, _yBound, 0), Quaternion.identity, this.transform);
+
+        }
+    }
+
+
+    public void StopSpawning()
+    {
+        StopCoroutine(_spawnEnemies);
+        StopCoroutine(_spawnPowerup);
+    }
 
 }
